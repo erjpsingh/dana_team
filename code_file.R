4d31c834efc12d595f278d49c19c1276e7b44e74
Jasp <erjpsingh@outlook.com>
2019-10-29 08:26:56 -0700

    second_commit, with the data files and initial pre-processing. Here I have imported data and taken a subset of it with the four required columns

---
+++ code_file.R
@@ -0,0 +1,44 @@
+#Setting the environment. 
+
+getwd()
+setwd('/home/jasp/DANA_4800/vancouver-crime-report/dana_team')
+
+
+#read data  this is of type list ie combination of vectors in R 
+data <- read.csv('crime_records.csv')
+
+#this is of type list ie combination of vectors in R 
+typeof(data)
+
+#selected particular data
+data_2 <- data[c('YEAR','HOUR','TYPE','NEIGHBOURHOOD')]
+
+#summarise our data
+summary(data_2)
+
+#remove NA in hour column, since there are many(61033) NA in the hour of the day 
+data_3 <- na.omit(data_2)
+
+summary(data_3)
+
+#write data to csv
+write.csv(data_3,"/home/jasp/DANA_4800/vancouver-crime-report/cleaned_data.csv")
+
+#crimes per year 
+year_temp <- table(data_3[1])
+barplot(year_temp)
+
+# crimes per neighbourhood
+
+neighbourhood_temp <- table((data_3[4]))
+barplot(neighbourhood_temp)  
+
+# crimes per category
+
+category_temp <- table(data_3[3])
+barplot(category_temp)
+
+# crimes per time of day
+
+time_of_day_temp <- table(data_3[2])
+barplot(time_of_day_temp)
