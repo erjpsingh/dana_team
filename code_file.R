e70f2d08155f62a6d4b703658a27beb84865ce6a
Jasp <erjpsingh@outlook.com>
2019-11-02 09:23:44 -0700

    We have removed partial years 2003 and 2019, and now we are all set to perform analysis on cleaned data

--- code_file.R
+++ code_file.R
@@ -21,24 +21,90 @@ data_3 <- na.omit(data_2)
 
 summary(data_3)
 
+#forming a new column Time_Of_Day based on hour column, here we are dividing 24 hours to 8 parts of day
+data_3$Time_Of_Day[data_3$HOUR >0 & data_3$HOUR <=3] <-1
+data_3$Time_Of_Day[data_3$HOUR >3 & data_3$HOUR <=6] <-2
+data_3$Time_Of_Day[data_3$HOUR >6 & data_3$HOUR <=9] <-3
+data_3$Time_Of_Day[data_3$HOUR >9 & data_3$HOUR <=12] <-4
+data_3$Time_Of_Day[data_3$HOUR >12 & data_3$HOUR <=15] <-5
+data_3$Time_Of_Day[data_3$HOUR >15 & data_3$HOUR <=18] <-6
+data_3$Time_Of_Day[data_3$HOUR >18 & data_3$HOUR <=21] <-7
+data_3$Time_Of_Day[data_3$HOUR >21 & data_3$HOUR <=24] <-8
+
+#subset data by removing 2003 and 2019 partial data 
+data_4 <- subset(data_3, YEAR >2003 & YEAR <2019)
+
+
 #write data to csv
 write.csv(data_3,"/home/jasp/DANA_4800/vancouver-crime-report/dana_team/cleaned_data.csv")
 
-#crimes per year 
-year_temp <- table(data_3[1])
-barplot(year_temp)
+summary(data_4)
 
-# crimes per neighbourhood
+#########################################################################################
+###                    VISUALIZATION                                                  ###
 
-neighbourhood_temp <- table((data_3[4]))
-barplot(neighbourhood_temp)  
+###UNIVARIATE 
 
-# crimes per category 
+# How have the quantity of reported crimes changed over the years?
+univariate_year_table <- table(data_3$YEAR)
+barplot(univariate_year_table,
+        main = "Rate of Crime over the years",
+        xlab='Years', 
+        ylab = 'Number of Crimes',
+        ylim = c(0,50000),
+        xaxp = c(300,700,5),
+        border = "white",
+        col = "tomato3")
 
-category_temp <- table(data_3[3])
-barplot(category_temp)
 
-# crimes per time of day
+# Which periods of each day tend to have the most total reported crimes?
 
-time_of_day_temp <- table(data_3[2])
-barplot(time_of_day_temp)
+time_of_day_table <- table(data_3$Time_Of_Day)
+barplot(time_of_day_table,
+        main = "Crimes as per period fo the day",
+        xlab='Time of day', 
+        ylab = 'Number of Crimes',
+        ylim = c(0,120000),
+        xaxp = c(300,700,5),
+        border = "white",
+        col = "tomato3")
+
+
+#Which types of crimes have been the most commonly reported?
+
+
+category_table <- table(data_3$TYPE)
+barplot(category_table,
+        main = "Crimes as per period fo the day",
+        xlab='Time of day', 
+        ylab = 'Type of Crimes',
+        ylim = c(0,210000),
+        xaxp = c(300,700,5),
+        border = "white",
+        col = "tomato3")
+
+
+
+
+# How many crimes have been reported in total over the population’s period for each neighbourhood?
+
+neighbourhood_table <- table((data_3$NEIGHBOURHOOD))
+barplot(neighbourhood_table,
+        main = "Crimes as per period fo the day",
+        xlab='Time of day', 
+        ylab = 'Type of Crimes',
+        ylim = c(0,210000),
+        xaxp = c(300,700,5),
+        border = "white",
+        col = "tomato3"        )  
+
+
+### BI-VARIATE
+
+# How does the time of day affect the type of crime reported?
+
+time_vs_type_of_crime_table <- table(data_3$Time_Of_Day, data_3$TYPE)
+barplot(time_of_day_table,
+        col=c("darkblue","red"),
+        beside = TRUE)
+
